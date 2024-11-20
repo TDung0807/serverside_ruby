364e9f3a403cd9800a03c100752ceacbfeb5f7f9
Trdung0807 <trungdungg0807@gmail.com>
2024-11-20 11:16:56 +0700

    add hash password

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+    def create 
+        @user = User.create(user_params)
+        if @user.save
+            render json:@user.as_json, status: :ok
+        else
+            render json:{message:"Error created", errors: @user.errors.full_messages}
+        end
+
+    end
+
+    private
+    def user_params
+        params.required(:user).permit(:email, :password, :name)
+    end
+end
